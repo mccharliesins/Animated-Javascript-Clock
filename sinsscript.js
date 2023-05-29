6e5900c9bfcf84821849e1a1c8a29dc372c637cd
Venkateshwar Yadav (McCharlie Sins) <vkyd1909@gmail.com>
2023-05-30 00:01:35 +0530

    few extra lines added

--- sinsscript.js
+++ sinsscript.js
@@ -10,6 +10,6 @@ setInterval(()=>{
     let ss = day.getSeconds()*deg;
 
     hr.style.transform = 'rotateZ(${(hh)*(mm/12)}deg)';
-    mn.style.transform = 'rotateZ(${mm}deg)';
-    sc.style.transform = 'rotateZ(${ss}deg)';
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
 })
