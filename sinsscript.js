09d9017141696831f291d5362d47876ba744f48b
Venkateshwar Yadav (McCharlie Sins) <vkyd1909@gmail.com>
2023-05-30 00:01:15 +0530

    testing a new section

--- sinsscript.js
+++ sinsscript.js
@@ -5,11 +5,11 @@ const sc=document.getElementById('sc');
 
 setInterval(()=>{
     let day = new Date();
-let hh = day.getHours()*30;
-let mm = day.getMinutes()*deg;
-let ss = day.getSeconds()*deg;
+    let hh = day.getHours()*30;
+    let mm = day.getMinutes()*deg;
+    let ss = day.getSeconds()*deg;
 
-hr.style.transform = 'rotateZ(${(hh)*(mm/12)}deg)';
-mn.style.transform = 'rotateZ(${mm}deg)';
-sc.style.transform = 'rotateZ(${ss}deg)';
+    hr.style.transform = 'rotateZ(${(hh)*(mm/12)}deg)';
+    mn.style.transform = 'rotateZ(${mm}deg)';
+    sc.style.transform = 'rotateZ(${ss}deg)';
 })
