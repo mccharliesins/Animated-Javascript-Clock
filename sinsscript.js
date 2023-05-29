70f2579f0efc5ea2e63773a863759848f32110d0
Venkateshwar Yadav (McCharlie Sins) <vkyd1909@gmail.com>
2023-05-29 23:51:55 +0530

    more bug fixes

--- sinsscript.js
+++ sinsscript.js
@@ -8,4 +8,6 @@ let hh = day.getHours()*30;
 let mm = day.getMinutes()*deg;
 let ss = day.getSeconds()*deg;
 
-hr.style.transform = 'rotateZ(${(hh)*(mm/12)}deg)'
+hr.style.transform = 'rotateZ(${(hh)*(mm/12)}deg)';
+mn.style.transform = 'rotateZ(${(hh)*(mm/12)}deg)';
+sc.style.transform = 'rotateZ(${(hh)*(mm/12)}deg)';
