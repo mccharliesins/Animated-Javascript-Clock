31b708b2aadeb9525121671ba36b5477e3ac06b9
Venkateshwar Yadav (McCharlie Sins) <vkyd1909@gmail.com>
2023-05-29 23:58:35 +0530

    bug fixes

--- sinsscript.js
+++ sinsscript.js
@@ -1,7 +1,7 @@
 const deg=6;
 const hr=document.getElementById('hr');
 const mn=document.getElementById('mn');
-const sc=document.querySelector('#sc');
+const sc=document.getElementById('sc');
 
 let day = new Date();
 let hh = day.getHours()*30;
